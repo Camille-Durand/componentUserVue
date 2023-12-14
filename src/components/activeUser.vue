aba3675acdb40698f4a3dc8c35de6ec7503ef177
Camille-Durand <camillefanguin@gmail.com>
2023-12-14 14:21:12 +0100

    modifs formulaire et age

--- src/components/activeUser.vue
+++ src/components/activeUser.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <ul class="list-group">
             <h2 class="list-group-item">Nom: {{ nom }}</h2>
-            <h3 class="list-group-item">Age: {{ age }} ans</h3>
+            <h3 class="list-group-item">Age: {{ age }} {{age < 2? 'an': 'ans'}}</h3>
         </ul>
     </div>
 </template>
--- src/components/userData.vue
+++ src/components/userData.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="container">
-        <input class="form-control form-control-sm" type="text" 
-        placeholder="Nom" aria-label=".form-control-sm example"
-        v-model="nom">
-        <input class="form-control form-control-sm" type="number" 
-        placeholder="Age" aria-label=".form-control-sm example"
-        v-model="age">
-        <button @click="submitData" type="button" class=" m-1 btn btn-outline-success">MAJ des Data</button>
+        <div class="list-group">
+            <h3>Formulaire:</h3>
+            <input class="form-control form-control-sm" type="text" 
+            placeholder="Nom" aria-label=".form-control-sm example"
+            v-model="nom">
+            <input class="form-control form-control-sm" type="number" 
+            placeholder="Age" aria-label=".form-control-sm example"
+            v-model="age">
+            <button @click="submitData" type="button" class=" m-1 btn btn-outline-success">MAJ des Data</button>
+        </div>
     </div>
 </template>
 
